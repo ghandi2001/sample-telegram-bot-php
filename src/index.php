5a351524adf7888510201d6e9151ab61001afa1e
_ghandi <a.ghandi2001@gmail.com>
2024-06-01 23:30:11 +0330

    wip commit.

--- src/index.php
+++ src/index.php
@@ -10,12 +10,29 @@ $bot = new Telebot($variables['TOKEN']);
 $bot->on('text', function ($data) use ($bot) {
     $chat_id = $data['chat']['id'];
     $text = $data['text'];
-    if (in_array($text, require 'codes.php')){
-        $bot->sendMessage(['chat_id' => $chat_id, 'text' => " کد تراکنش $text معتبر است"]);
-    }else{
-        $bot->sendMessage(['chat_id' => $chat_id, 'text' => "پیام نا معتبر"]);
+    $codes = require 'codes.php';
+    if (array_key_exists($text, $codes)) {
+        $price = number_format($codes[$text]['amount']);
+        $bot->sendMessage(['chat_id' => $chat_id, 'text' =>
+            <<<TXT
+کد تراکنش $text معتبر است
+مبلغ : {$price}
+از : {$codes[$text]['src']} 
+به : {$codes[$text]['dst']}
+\n
+TXT
+        ]);
+        $bot->sendMessage(['chat_id' => $chat_id, 'text' => "لطفا برای ادامه کد مورد نظر بعدی را وارد کنید !!!"]);
+    } else {
+        $bot->sendMessage(['chat_id' => $chat_id, 'text' => "کد تراکنش نا معتبر"]);
+        $bot->sendMessage(['chat_id' => $chat_id, 'text' => "لطفا کد تراکنش مورد نظر را وارد کنید ..."]);
     }
-
 });
+
+
+/*$bot->on('*',function ($type, $data) use ($bot) {
+    $chat_id = $data['chat']['id'];
+    $bot->sendMessage(['chat_id' => $chat_id, 'text' => "لطفا کد تراکنش مورد نظر را وارد کنید ..."]);
+});*/
 
 $bot->run();
